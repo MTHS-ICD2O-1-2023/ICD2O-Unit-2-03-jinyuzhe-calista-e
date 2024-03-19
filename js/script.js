4f0e21d0108f4d32239479b7293299ff06621278
Calista.E <159185314+jinyuzhe-calista-e@users.noreply.github.com>
2024-03-19 18:39:31 +0000

    done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Calista.E All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Calista.E
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+
+/**
+ * This function gets users first name and age and shows it back to user.
+ */
+function enterClicked() {
+  // input
+  const Streetname = document.getElementById("Street-name").value
+  const Streetnumber = parseInt(document.getElementById("Street-number").value)
+
+  // output
+  document.getElementById("user-info").innerHTML =
+    "Your address is: " + Streetnumber + " " + Streetname + "."
+}
